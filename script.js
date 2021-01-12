b174f6075716043c8a3bff6f2c6d6ea4b718b953
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-12 13:11:59 +0530

    (feat): add normal maps

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 var geometry = new THREE.BoxGeometry(1, 1, 1);
-var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+var material = new THREE.MeshNormalMaterial();
 var cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
