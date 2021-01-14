5274c2f802af16a51c5fcc4851a0ec191777c85a
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-01-14 23:07:28 +0530

    (feat): init pop

---
+++ pop/script.js
@@ -0,0 +1,36 @@
+window.onload = function () {
+  // get DOM
+  var canvas = document.getElementById("viewport");
+  var context = canvas.getContext("2d");
+
+  // declare variables
+  var p1_score = 0;
+  var p2_score = 0;
+  var time = 0;
+
+  // Initialize the game
+  function init() {
+    // Add mouse events
+    canvas.addEventListener("click", onClick);
+
+    // get main loop
+    main(0);
+  }
+
+  // Main loop
+  function main(tframe) {
+    render();
+  }
+
+  // Render the game
+  function render() {
+    // Draw the frame
+    drawFrame();
+  }
+
+  // add mouse events
+  function onClick(e) {}
+
+  // init
+  init();
+};
